81a08250bb96d1398a1780f2ad2775b05982e045
bluec0re <coding@bluec0re.eu>
2019-10-02 20:42:45 +0200

    WIP: pytest based testing

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-if [[ ! "${BASEDIR}" == "/app/tests" ]]; then
-  echo "[!] The tests currently require a MySQL backend."
-  echo "[!] Please execute this instead through Docker with:"
-  echo -e "\tdocker/docker-admin.sh test"
-  exit
-fi
-
 # Make sure we are in the project root directory before executing the test runner.
 cd ${BASEDIR}/../
 
@@ -17,4 +10,5 @@ pytest \
   --cov=app \
   --cov=lib \
   --cov=data \
+  -m 'not integration' \
   tests
--- tests/test_base.py
+++ tests/test_base.py
@@ -11,17 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Basis for all Flask related unit tests."""
-
-import unittest
-import os
-import cfg
+import pytest
 from lib.app_factory import create_app
-from flask_migrate import upgrade as alembic_upgrade
-from data.database import db
-
-with open(os.path.join(cfg.BASE_DIR, "docker/db_schema.sql"), "rb") as f:
-    _create_schemas_sql = f.read().decode("utf8")
 
 DOCKER_DB_URI = 'mysql+mysqldb://root:test_db_pass@tests-db:3306/main'
 TEST_CONFIG = {
@@ -31,32 +22,16 @@ TEST_CONFIG = {
     'SQLALCHEMY_DATABASE_URI': None
 }
 
+@pytest.fixture
+def app():
+    app = create_app(TEST_CONFIG)
+    return app
 
-class FlaskTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        app = create_app(TEST_CONFIG)
-        cls.app = app
-        cls.test_client = app.test_client()
-
-        # Push one single global flask app context for usage.
-        ctx = app.app_context()
-        ctx.push()
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
 
-class FlaskIntegrationTest(FlaskTest):
-    @classmethod
-    def setUpClass(cls):
-        TEST_CONFIG['SQLALCHEMY_DATABASE_URI'] = DOCKER_DB_URI
-        super(FlaskIntegrationTest, cls).setUpClass()
-
-        # Create all required schemas.
-        db.engine.execute(_create_schemas_sql)
-        # Create all data from the alembic migrations.
-        alembic_upgrade()
+def test_index(client):
+  resp = client.get('/')
+  assert resp.status == 200
