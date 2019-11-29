102fd660d4d3426185b35188d63203c598780489
bluec0re <coding@bluec0re.eu>
2019-11-29 15:50:23 +0100

    added secret key for testing

--- tests/run_unit_tests.sh
+++ tests/run_unit_tests.sh
@@ -6,6 +6,8 @@ BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 # Make sure we are in the project root directory before executing the test runner.
 cd ${BASEDIR}/../
 
+export COOKIE_SECRET_KEY=not-so-secret
+
 pytest \
   --cov-config=.coveragerc \
   --cov-report html \
