0eb20cb161639b9d7f7533092722186ae3482d46
bluec0re <coding@bluec0re.eu>
2020-10-23 18:38:44 +0200

    Implemented change detection.
    Fixes #48

--- data/models/base.py
+++ data/models/base.py
@@ -11,12 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from typing import Dict, Tuple, Any, TYPE_CHECKING
+from itertools import zip_longest
+import logging
 
 from flask_sqlalchemy import SQLAlchemy as SQLAlchemyBase  # type: ignore
 from flask_sqlalchemy import DefaultMeta
 from flask_marshmallow import Marshmallow  # type: ignore
-from sqlalchemy import Index, Column, Integer, func, DateTime
+from sqlalchemy import Index, Column, Integer, func, DateTime, inspect
 from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import Mapper, RelationshipProperty
+
+log = logging.getLogger(__name__)
 
 
 # Adding the "pool_pre_ping" command to avoid mysql server has gone away issues.
@@ -49,6 +55,62 @@ class MainBase(BaseModel):
         default=func.current_timestamp(),
         onupdate=func.current_timestamp(),
     )
+
+    def diff(self,
+             other: BaseModel,
+             *,
+             already_tested=None) -> Dict[str, Tuple[Any, Any]]:
+        changes = {}
+        if already_tested is None:
+            already_tested = {id(self), id(other)}
+        elif id(self) in already_tested and id(other) in already_tested:
+            return changes
+        already_tested.add(id(self))
+        already_tested.add(id(other))
+        clz = type(self)
+        oclz = type(other)
+        if not isinstance(other, clz):
+            raise TypeError("Instance of {} expected. Got {}".format(
+                clz.__name__, oclz.__name__))
+
+        def innerdiff(current, other):
+            if current is None and other is None:
+                return None
+            elif current is None or other is None:
+                return (current, other)
+            elif hasattr(current, 'diff'):
+                return current.diff(other, already_tested=already_tested)
+            elif isinstance(current, list) and isinstance(other, list):
+                res = []
+                for c, o in zip_longest(current, other):
+                    res.append(innerdiff(c, o))
+                return res
+            elif current != other:
+                return (cv, ov)
+
+        m: Mapper = inspect(clz)
+        for name, attr in m.attrs.items():
+            # log.debug('Compare %s of %s <> %s', name, clz, oclz)
+            ov = getattr(other, name)
+            cv = getattr(self, name)
+
+            if name == 'resources':
+                breakpoint()
+
+            if isinstance(attr, RelationshipProperty):
+                for c in attr.local_columns:
+                    cname = c.name
+                    if innerdiff(getattr(self, cname), getattr(other, cname)):
+                        break
+                else:
+                    continue
+                if name in changes:
+                    continue
+            c = innerdiff(cv, ov)
+            if c:
+                changes[name] = c
+
+        return changes
 
 
 class NvdBase(BaseModel):
