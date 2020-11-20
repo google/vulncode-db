6d88cf24d00c7a7dbab71c87b5cf5de0713d1af1
bluec0re <coding@bluec0re.eu>
2020-11-20 10:46:40 +0100

    fix permission errors for reviewers

--- app/review/routes.py
+++ app/review/routes.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 from sqlakeyset import get_page  # type: ignore
 from sqlakeyset.results import s as sqlakeysetserial  # type: ignore
-from sqlalchemy import desc, asc, case
+from sqlalchemy import desc, asc, or_
 from bouncer.constants import READ  # type: ignore
 
 from app.auth.acls import requires
@@ -45,20 +45,45 @@ sqlakeysetserial.custom_unserializations = {
 }
 
 
-# Create a catch all route for profile identifiers.
-@bp.route("/list")
-@requires(READ, 'Proposal')
-def list(vendor: str = None, profile: str = None):
+def get_pending_proposals_paged():
     entries = db.session.query(Vulnerability, Nvd)
     entries = entries.filter(
         Vulnerability.state != VulnerabilityState.PUBLISHED)
     entries = entries.outerjoin(Vulnerability,
                                 Nvd.cve_id == Vulnerability.cve_id)
     entries = entries.order_by(asc(Vulnerability.state), desc(Nvd.id))
-
     bookmarked_page = parse_pagination_param("review_p")
     per_page = 10
     entries_full = entries.options(default_nvd_view_options)
     review_vulns = get_page(entries_full, per_page, page=bookmarked_page)
     review_vulns = VulnViewTypesetPaginationObjectWrapper(review_vulns.paging)
-    return render_template("review/list.html", review_vulns=review_vulns)
+    return review_vulns
+
+
+def get_reviewed_proposals_paged():
+    entries = db.session.query(Vulnerability, Nvd)
+    entries = entries.filter(
+        or_(Vulnerability.state == VulnerabilityState.PUBLISHED,
+            Vulnerability.state == VulnerabilityState.REVIEWED,
+            Vulnerability.state == VulnerabilityState.ARCHIVED),
+        Vulnerability.reviewer == g.user)
+    entries = entries.outerjoin(Vulnerability,
+                                Nvd.cve_id == Vulnerability.cve_id)
+    entries = entries.order_by(asc(Vulnerability.state), desc(Nvd.id))
+    bookmarked_page = parse_pagination_param("reviewed_p")
+    per_page = 10
+    entries_full = entries.options(default_nvd_view_options)
+    review_vulns = get_page(entries_full, per_page, page=bookmarked_page)
+    review_vulns = VulnViewTypesetPaginationObjectWrapper(review_vulns.paging)
+    return review_vulns
+
+
+# Create a catch all route for profile identifiers.
+@bp.route("/list")
+@requires(READ, 'Proposal')
+def list(vendor: str = None, profile: str = None):
+    review_vulns = get_pending_proposals_paged()
+    reviewed_vulns = get_reviewed_proposals_paged()
+    return render_template("review/list.html",
+                           review_vulns=review_vulns,
+                           reviewed_vulns=reviewed_vulns)
